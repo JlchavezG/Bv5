a4439a7b2e30515ceeaf90749e4d85408207f8e3
JlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-07-20 22:29:44 -0500

    elimando codigo
    se elimino gran parte del codigo de grid la practica pasada

--- index.php
+++ index.php
@@ -7,43 +7,7 @@
   </head>
   <body>
       <div class="container">
-        <!-- linea 1 -->
-        <div class="row bg-danger text-center">
-          <div class="col-sm-1 col-md-1 col-lg-1">1</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">2</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">3</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">4</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">5</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">6</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">7</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">8</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">9</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">10</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">11</div>
-          <div class="col-sm-1 col-md-1 col-lg-1">12</div>
-        </div>
-        <!-- linea 2 -->
-        <div class="row bg-primary text-cente">
-           <div class="col-sm-4 col-md-4 col-lg-4">1</div>
-           <div class="col-sm-4 col-md-4 col-lg-4">2</div>
-           <div class="col-sm-4 col-md-4 col-lg-4">3</div>
-        </div>
-        <!-- linea 3 -->
-        <div class="row bg-warning text-center">
-            <div class="col-sm-4 col-md-4 col-lg-4">1</div>
-            <div class="col-sm-8 col-md-8 col-lg-8">1</div>
-        </div>
-        <!-- linea 4 -->
-        <div class="row bg-secondary text-center">
-            <div class="col-sm-6 col-md-6 col-lg-6">1</div>
-            <div class="col-sm-6 col-md-6 col-lg-6">2</div>
-        </div>
-        <!-- linea 5 -->
-        <div class="row bg-info text-center">
-           <div class="col-sm-12 col-md-12 col-lg-12">
-              1
-           </div>
-        </div>
+        
       </div>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
